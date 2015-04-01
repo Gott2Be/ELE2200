b997a09dff8603cd977f96a6a90b9823ce743948
Antonio Sanniravong <antonio.sanniravong@polymtl.ca>
2015-04-01 16:04:50 -0400

    Lab03 presque fini

--- Lab03/lab03.m
+++ Lab03/lab03.m
@@ -12,21 +12,23 @@ F = simplify(C*inv(s*I-A)*B+D)
 clear all
 s = tf('s')
 
-L = 0.00025
-R = 0.5
-Kc = 0.48
-Km = 0.5
-Bm = 0.02
-Jm = 0.05
+L = 0.00025;
+R = 0.5;
+Kc = 0.48;
+Km = 0.5;
+Bm = 0.02;
+Jm = 0.05;
 
-A=[-R/L -Kc/L ; Km/Jm -Bm/Jm]
-B=[1/L ; 0]
-C=[0 1]
-D=[0]
-I=eye(2)
+A=[-R/L -Kc/L ; Km/Jm -Bm/Jm];
+B=[1/L ; 0];
+C=[0 1];
+D=[0];
+I=eye(2);
 
-F = C*inv(s*I-A)*B+D
+F = C*inv(s*I-A)*B+D;
+fprintf('ZPK:\n:')
 zpk(F)
+fprintf('Pole:\n:')
 pole(F)
 
 %%
@@ -63,7 +65,7 @@ pole(Fa)
 %% 2.2 moteur + bras
 clear all
 %s = tf('s')
-syms thetae s
+syms thetae B1 J1 l1 s
 
 L = 0.00025
 R = 0.5
@@ -71,11 +73,11 @@ Kc = 0.48
 Km = 0.5
 Bm = 0.02
 Jm = 0.05
-l1 = 0.3
+%l1 = 0.3
 M1 = 2
-B1 = 0.125
+%B1 = 0.125
 g = 9.81
-J1 = 0
+%J1 = 0
 
 A=[-R/L 0 -Kc/L
     0 0 1
@@ -102,11 +104,73 @@ syms Bm Jm
 R = 0.5;
 Km = 0.5;
 Kc = 0.48;
-Fa = Km/(Bm*R + Kc*Km + Jm*R*s)
-interp1(ScopeData(:,2), ScopeData(:,1), 0.63*2)
+%Fa = Km/(Bm*R + Kc*Km + Jm*R*s)
+tau = interp1(ScopeData(:,2), ScopeData(:,1), 0.63*2)
 
-%% 
+plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), 0.63*2, 'r', tau, 0.63*2, 'b*')
 
+%% 4.1 Schema de simulation
 
+L = 0.00025
+R = 0.5
+Kc = 0.48
+Km = 0.5
+Bm = 0.02
+Jm = 0.05
+l1 = 0.3
+M1 = 2
+B1 = 0.125
+g = 9.81
+J1 = 0.06
+thetae = pi/4
 
+A=[-R/L 0 -Kc/L
+    0 0 1
+    Km/(Jm+J1) -M1*g*l1*thetae*cos(thetae)/(2*(Jm+J1)) -(Bm+B1)/(Jm+J1)]
+B = [1/L ; 0 ; 0]
+C = [0 1 0]
+D = [0]
 
+%% 4.2 Perturbation condition initiale
+
+perturbation_theta = 5;
+sim('Schema_de_simulation_42')
+subplot(3,2,1)
+plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
+
+perturbation_theta = 15;
+sim('Schema_de_simulation_42')
+subplot(3,2,3)
+plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
+
+perturbation_theta = 30;
+sim('Schema_de_simulation_42')
+subplot(3,2,5)
+plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
+
+perturbation_theta = -5;
+sim('Schema_de_simulation_42')
+subplot(3,2,2)
+plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
+
+perturbation_theta = -15;
+sim('Schema_de_simulation_42')
+subplot(3,2,4)
+plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
+
+perturbation_theta = -30;
+sim('Schema_de_simulation_42')
+subplot(3,2,6)
+plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
+thetae = pi/4;
+
+A=[-R/L 0 -Kc/L
+    0 0 1
+    Km/(Jm+J1) -M1*g*l1*thetae*cos(thetae)/(2*(Jm+J1)) -(Bm+B1)/(Jm+J1)]
+B = [1/L ; 0 ; 0]
+C = [0 1 0]
+D = [0]
+
+%% 4.3 Perturbation entree
+perturbation_theta = 0;
+perturbation_u = 0.05;
