0084aa00500f79b9a6c572ef9957e44e1e31d010
Antonio Sanniravong <antonio.sanniravong@polymtl.ca>
2015-04-15 16:10:20 -0400

    Modifs de lab 03 post rapport

--- Lab03/lab03.m
+++ Lab03/lab03.m
@@ -99,7 +99,7 @@ D = [0]
 I=eye(3)
 
 Ga = simplify(C*inv(s*I-A)*B+D)
-%% 3
+%% 3.1
 syms Bm Jm
 R = 0.5;
 Km = 0.5;
@@ -107,7 +107,21 @@ Kc = 0.48;
 %Fa = Km/(Bm*R + Kc*Km + Jm*R*s)
 tau = interp1(ScopeData(:,2), ScopeData(:,1), 0.63*2)
 
-plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), 0.63*2, 'r', tau, 0.63*2, 'b*')
+plot(ScopeData(:,1), ScopeData(:,2), tau, 0.63*2, 'b*')
+title('Gain statique et temps de montée')
+xlabel('Temps (s)')
+ylabel('\omega (rad/s)')
+
+%% 3.2
+delta_theta_max = max(theta(:,2))
+Tp = theta((find(theta(:,2) == delta_theta_max)),1)
+delta_theta_rp = theta(end)
+delta_u = 0.017298519974968
+
+plot(theta(:,1), theta(:,2))
+title('Réponse du système en équilibre à une perturbation de 1% en entrée.')
+xlabel('Temps (s)')
+ylabel('\omega (rad/s)')
 
 %% 4.1 Schema de simulation
 
@@ -132,38 +146,59 @@ C = [0 1 0]
 D = [0]
 
 %% 4.2 Perturbation condition initiale
+perturbation_u = 0;
 
-perturbation_theta = 5;
+figure(1)
+perturbation_theta = -30;
 sim('Schema_de_simulation_42')
-subplot(3,2,1)
+subplot(3,1,1)
 plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
-
-perturbation_theta = 15;
-sim('Schema_de_simulation_42')
-subplot(3,2,3)
-plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
-
-perturbation_theta = 30;
-sim('Schema_de_simulation_42')
-subplot(3,2,5)
-plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
-
-perturbation_theta = -5;
-sim('Schema_de_simulation_42')
-subplot(3,2,2)
-plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
+titre = sprintf('Réponse du système à une perturbation de %d degrés par rapport au point d''équilibre.', perturbation_theta)
+title(titre)
+legend('Non-linéaire', 'Linéarisé', 'Location', 'best');
 
 perturbation_theta = -15;
 sim('Schema_de_simulation_42')
-subplot(3,2,4)
+subplot(3,1,2)
 plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
+titre = sprintf('Réponse du système à une perturbation de %d degrés par rapport au point d''équilibre.', perturbation_theta)
+title(titre)
+legend('Non-linéaire', 'Linéarisé', 'Location', 'best');
 
-perturbation_theta = -30;
+perturbation_theta = -5;
 sim('Schema_de_simulation_42')
-subplot(3,2,6)
+subplot(3,1,3)
 plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
-thetae = pi/4;
+titre = sprintf('Réponse du système à une perturbation de %d degrés par rapport au point d''équilibre.', perturbation_theta)
+title(titre)
+legend('Non-linéaire', 'Linéarisé', 'Location', 'best');
 
+figure(2)
+perturbation_theta = 5;
+sim('Schema_de_simulation_42')
+subplot(3,1,1)
+plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
+titre = sprintf('Réponse du système à une perturbation de %d degrés par rapport au point d''équilibre.', perturbation_theta)
+title(titre)
+legend('Non-linéaire', 'Linéarisé', 'Location', 'best');
+
+perturbation_theta = 15;
+sim('Schema_de_simulation_42')
+subplot(3,1,2)
+plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
+titre = sprintf('Réponse du système à une perturbation de %d degrés par rapport au point d''équilibre.', perturbation_theta)
+title(titre)
+legend('Non-linéaire', 'Linéarisé', 'Location', 'best');
+
+perturbation_theta = 30;
+sim('Schema_de_simulation_42')
+subplot(3,1,3)
+plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
+titre = sprintf('Réponse du système à une perturbation de %d degrés par rapport au point d''équilibre.', perturbation_theta)
+title(titre)
+legend('Non-linéaire', 'Linéarisé', 'Location', 'best');
+
+thetae = pi/4;
 A=[-R/L 0 -Kc/L
     0 0 1
     Km/(Jm+J1) -M1*g*l1*thetae*cos(thetae)/(2*(Jm+J1)) -(Bm+B1)/(Jm+J1)]
@@ -174,3 +209,32 @@ D = [0]
 %% 4.3 Perturbation entree
 perturbation_theta = 0;
 perturbation_u = 0.05;
+
+figure(1)
+perturbation_u = 0.05;
+sim('Schema_de_simulation_42')
+subplot(3,1,1)
+plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
+title('Réponse du système à une perturbation de 5% par rapport à la tension d''équilibre.')
+legend('Non-linéaire', 'Linéarisé', 'Location', 'best');
+
+perturbation_u = 0.1;
+sim('Schema_de_simulation_42')
+subplot(3,1,2)
+plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
+title('Réponse du système à une perturbation de 10% par rapport à la tension d''équilibre.')
+legend('Non-linéaire', 'Linéarisé', 'Location', 'best');
+
+perturbation_u = 0.25;
+sim('Schema_de_simulation_42')
+subplot(3,1,3)
+plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
+title('Réponse du système à une perturbation de 25% par rapport à la tension d''équilibre.')
+legend('Non-linéaire', 'Linéarisé', 'Location', 'best');
+
+figure(2)
+perturbation_u = 0.45;
+sim('Schema_de_simulation_42')
+plot(ScopeData(:,1), ScopeData(:,2), ScopeData(:,1), ScopeData(:,3))
+title('Réponse du système à une perturbation de 45% par rapport à la tension d''équilibre.')
+legend('Non-linéaire', 'Linéarisé', 'Location', 'best');
